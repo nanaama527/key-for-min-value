394c2e44d955ce6ef708a3d89e64b6ca29c5f0a9
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-14 22:51:16 -0500

    commit tree 607034c7c34a8dbbb47ea1f35892da9adf0181ab

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+   number = 1000
+   result = nil
+   name_hash.each do |key, value|
+    if value < number
+        number = value
+        result = key
+    end
+end
+    return result
 end
